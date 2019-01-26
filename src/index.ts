9c5729a79b505bd6ff2b9a2e215609e2e4ba57e9
kjirou <sorenariblog@gmail.com>
2019-01-26 18:17:33 +0900

    Create a function to read the directions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-export interface ExecutionResult {
-  exitCode: number,
-  outputErrorMessage: string,
-}
+const fs = require('fs');
 
 // 1) READMEからクエリを探す
 // 2) 要求するファイルパスリストを生成
@@ -10,12 +7,42 @@ export interface ExecutionResult {
 // 5) .jsリストがREADMEでどの位置にあるかを探し出してそれぞれ挿入する
 // 6) 置換されたREADMEを標準出力
 
+export interface EmbeddingDirection {
+  directionStartIndex: number,
+  directionEndIndex: number,
+  filePath: string,
+}
+
+function searchEmbeddingDirections(readmeText: string): EmbeddingDirection[] {
+  const regExp = /<!-- *embed-examples: *(.+?) *-->/g;
+  const directions = [];
+  let matched;
+  while (matched = regExp.exec(readmeText)) {
+    directions.push({
+      directionStartIndex: matched.index,
+      directionEndIndex: regExp.lastIndex - 1,
+      filePath: matched[1],
+    });
+  }
+  return directions;
+}
+
+export interface ExecutionResult {
+  exitCode: number,
+  outputErrorMessage: string,
+}
+
 export function execute(
   moduleName: string,
   mainModuleIdUsedInExample: string,
   readmeFilePath: string,
   examplesDirPath: string
 ): Promise<ExecutionResult> {
+  const readmeText = fs.readFileSync(readmeFilePath).toString();
+  // TODO: Handle a failure to read
+  const directions = searchEmbeddingDirections(readmeText);
+  console.log(directions);
+
   return Promise.resolve()
     .then(() => {
       if (moduleName === 'e') {
