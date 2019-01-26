9327a944f1b63d310e7149a253c907d7659bbac8
kjirou <sorenariblog@gmail.com>
2019-01-26 17:36:31 +0900

    Note rough design

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@ export interface ExecutionResult {
   outputErrorMessage: string,
 }
 
+// 1) READMEからクエリを探す
+// 2) 要求するファイルパスリストを生成
+// 3) ファイルパスリストから.jsリストを読み込む
+// 4) .jsリストのmainModuleIdUsedInExampleをmoduleNameに置換
+// 5) .jsリストがREADMEでどの位置にあるかを探し出してそれぞれ挿入する
+// 6) 置換されたREADMEを標準出力
+
 export function execute(
   moduleName: string,
   mainModuleIdUsedInExample: string,
