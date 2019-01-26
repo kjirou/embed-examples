07b7a45ceb7f77ef5e654576b7356f2f9ffbfecf
kjirou <sorenariblog@gmail.com>
2019-01-26 17:19:44 +0900

    Define an api to connect CLI environment

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const minimist = require('minimist');
 const path = require('path');
 
 let embedExamples;
@@ -12,4 +13,24 @@ if (fs.existsSync(path.join(__dirname, '../dist/index.js'))) {
   embedExamples = require('../src');
 }
 
-console.log(embedExamples.dummy());
+const cwd = process.cwd();
+
+const parsedArgv = minimist(process.argv.slice(2));
+const [
+  moduleName = '',
+  relativeInputDirPath = '',
+  relativeOutputFilePath = '',
+] = parsedArgv._;
+
+// TODO: Validate args
+
+const inputDirPath = path.join(cwd, relativeInputDirPath);
+const outputFilePath = path.join(cwd, relativeOutputFilePath);
+
+embedExamples.execute(moduleName, inputDirPath, outputFilePath)
+  .then(results => {
+    if (results.exitCode > 0) {
+      process.stdout.write(`${results.outputErrorMessage}\n`);
+    }
+    process.exit(results.exitCode)
+  });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
-export const dummy = () => {
-  const x: number = 1;
-  return x;
+export interface ExecutionResult {
+  exitCode: number,
+  outputErrorMessage: string,
 }
+
+export function execute(moduleName: string, inputDir: string, outputFile: string): Promise<ExecutionResult> {
+  return Promise.resolve()
+    .then(() => {
+      if (moduleName === 'e') {
+        return {
+          exitCode: 1,
+          outputErrorMessage: 'ERROR!',
+        };
+      } else {
+        return {
+          exitCode: 0,
+          outputErrorMessage: '',
+        };
+      }
+    });
+};
