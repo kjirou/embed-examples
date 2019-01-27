67745f89a71d407f4dbf39bf921b7b3d0e9ac81f
kjirou <sorenariblog@gmail.com>
2019-01-27 11:00:15 +0900

    Insert newline characters around code-block notation

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,16 @@ function escapeRegExp(input: string): string {
   return input.replace(/[.*+?^=!:${}()|[\]\/\\]/g, '\\$&');
 }
 
+type NewlineCharacterSetting = 'LF' | 'CR' | 'CRLF';
+
+function getNewlineCharacter(newlineCharacterSetting: NewlineCharacterSetting): string {
+  return {
+    LF: '\n',
+    CR: '\r',
+    CRLF: '\r\n',
+  }[newlineCharacterSetting];
+}
+
 interface EmbeddingDirection {
   directionStartIndex: number,
   directionEndIndex: number,
@@ -23,7 +33,9 @@ function searchEmbeddingDirections(readmeText: string): EmbeddingDirection[] {
     [
       '(<!-- *embed-examples: *(.+?) *-->)',
       '(?:',
-      escapeRegExp(EMBEDDED_EXAMPLE_START_TAG) + '```(?:\r\n|[\n\r]|.)+?```' + escapeRegExp(EMBEDDED_EXAMPLE_END_TAG),
+      escapeRegExp(EMBEDDED_EXAMPLE_START_TAG) + '(?:\r\n|[\n\r])',
+      '```(?:\r\n|[\n\r]|.)+?```(?:\r\n|[\n\r])',
+      escapeRegExp(EMBEDDED_EXAMPLE_END_TAG),
       ')?',
     ].join(''),
     'g'
@@ -80,20 +92,18 @@ function embedExamplesIntoReadme(
   readmeText: string,
   directions: EmbeddingDirection[],
   examples: ExampleSourcesMap,
-  newlineCharacter: 'LF' | 'CR' | 'CRLF',
+  newlineCharacterSetting: NewlineCharacterSetting,
 ): string {
   const directionsOrderdFromTail =
     directions.slice().sort((a, b) => b.directionStartIndex - a.directionStartIndex);
-  const newlineCharacterText = {
-    LF: '\n',
-    CR: '\r',
-    CRLF: '\r\n',
-  }[newlineCharacter];
+  const newlineCharacter = getNewlineCharacter(newlineCharacterSetting);
   let newReadmeText = readmeText;
   directionsOrderdFromTail.forEach(direction => {
     newReadmeText = newReadmeText.slice(0, direction.directionStartIndex) +
       direction.directionBody +
-      EMBEDDED_EXAMPLE_START_TAG + '```' + newlineCharacterText + examples[direction.filePath] + '```' + EMBEDDED_EXAMPLE_END_TAG +
+      EMBEDDED_EXAMPLE_START_TAG + newlineCharacter +
+      '```' + newlineCharacter + examples[direction.filePath] + '```' + newlineCharacter +
+      EMBEDDED_EXAMPLE_END_TAG +
       newReadmeText.slice(direction.directionEndIndex + 1);
   });
   return newReadmeText;
@@ -103,12 +113,12 @@ export function execute(
   readmeText: string,
   examplesDirPath: string,
   replacementKeywords: ReplacementKeyword[],
-  newlineCharacter: 'LF' | 'CR' | 'CRLF',
+  NewlineCharacterSetting: NewlineCharacterSetting,
 ): string {
   const directions = searchEmbeddingDirections(readmeText);
   const exampleSourcesMap = replaceKeywords(
     fetchExamples(examplesDirPath, directions),
     replacementKeywords
   );
-  return embedExamplesIntoReadme(readmeText, directions, exampleSourcesMap, newlineCharacter);
+  return embedExamplesIntoReadme(readmeText, directions, exampleSourcesMap, NewlineCharacterSetting);
 };
